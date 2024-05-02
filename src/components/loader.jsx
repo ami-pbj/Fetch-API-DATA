d3b9fb4dbc10ce3c7a4f1018012723feb8463579
PURNENDU BIKASH JANA <purnendubikash.21@gmail.com>
2024-05-03 00:24:19 +0530

    Fixed Loader

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex flex-col justify-center items-center py-3">
+    <div className="flex flex-col justify-center items-center py-3 m-[4rem]">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-green-700" />
       <p className="text-[22px] font-medium mt-5">Fetching Data ...</p>
     </div>
